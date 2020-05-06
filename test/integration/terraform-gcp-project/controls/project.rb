05760ac7c1ea0f10425f4a086d1e4cc060d235e6
Isabel Restrepo <isabelrestre@gmail.com>
2020-05-06 12:02:57 -0400

    fix: format of service account information on tests

--- test/integration/terraform-gcp-project/controls/project.rb
+++ test/integration/terraform-gcp-project/controls/project.rb
@@ -6,12 +6,6 @@ project_id = attribute("project_id")
 
 service_account_email = attribute("service_account_email")
 
-# # content = inspec.profile.file("terraform.json")
-# # params = JSON.parse(content)
-
-# # PROJECT_ID = params['project_id']['value']
-# # SERVICE_ACCOUNT_EMAIL = params['metadatawriter']['value']
-
 describe google_project(project: project_id) do
   it { should exist }
   its('project_id') { should eq project_id }
@@ -22,22 +16,17 @@ describe google_project(project: project_id) do
 end
 
 
-# describe google_project_iam_binding(project: project_id) do
-#   it { should exist }
-#   its('members') {should include service_account_email }
-# end
-
 describe google_project_iam_binding(project: project_id,  role: 'roles/stackdriver.resourceMetadata.writer') do
   it { should exist }
-  its('members') {should include service_account_email }
+  its('members') {should include "serviceAccount:#{service_account_email}" }
 end
 
-# describe google_project_iam_binding(project: gcp_project_id,  role: 'roles/monitoring.metricWriter') do
-#   it { should exist }
-#   its('members') {should include service_account_email }
-# end
+describe google_project_iam_binding(project: project_id,  role: 'roles/monitoring.metricWriter') do
+  it { should exist }
+  its('members') {should include "serviceAccount:#{service_account_email}" }
+end
 
-# describe google_project_iam_binding(project: gcp_project_id,  role: 'roles/logging.logWriter') do
-#   it { should exist }
-#   its('members') {should include service_account_email }
-# end
+describe google_project_iam_binding(project: project_id,  role: 'roles/logging.logWriter') do
+  it { should exist }
+  its('members') {should include "serviceAccount:#{service_account_email}" }
+end
