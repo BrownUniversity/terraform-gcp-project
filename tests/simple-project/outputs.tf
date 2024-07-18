2ef2d245cebc5d21b688cd9faaea4e5f169e4fab
Paul Xu <yang_xu@brown.edu>
2024-07-17 22:59:51 -0400

    write more tests

--- tests/simple-project/outputs.tf
+++ tests/simple-project/outputs.tf
@@ -1,44 +1,35 @@
 output "project_name" {
-  description = "Project Name"
-  value       = module.project.project_name
+  value = module.simple-project.project_name
 }
 
 output "project_id" {
-  description = "Project ID"
-  value       = module.project.project_id
+  value = module.simple-project.project_id
 }
 
 output "service_account_display_name" {
-  description = "Name of service account"
-  value       = module.project.service_account_display_name
+  value = module.simple-project.service_account_display_name
 }
 
 output "service_account_email" {
-  description = "Email for service account"
-  value       = module.project.service_account_email
+  value = module.simple-project.service_account_email
 }
 
 output "enabled_apis" {
-  description = "Active Google APIS"
-  value       = module.project.enabled_apis
+  value = module.simple-project.enabled_apis
 }
 
 output "roles" {
-  description = "Roles"
-  value       = module.projects_iam_bindings.roles
+  value = module.simple-project.roles
 }
 
 output "activate_apis" {
-  description = "Activate APIs"
-  value       = var.activate_apis
+  value = var.activate_apis
 }
 
-output "org_id" {
-  description = "Organization ID"
-  value       = var.org_id
+output "organization_id" {
+  value = var.org_id
 }
 
 output "folder_id" {
-  description = "Folder ID"
-  value       = var.folder_id
+  value = var.folder_id
 }
